3e797c74bfcfcd662e403e680892148819b59014
rodrigo-m-campos <rcamposreggio@gmail.com>
2025-10-08 12:08:10 +0200

    Fixed indentation

--- code.sql
+++ code.sql
@@ -1,103 +1,103 @@
 --Create the tables
 CREATE TABLE AppUser (
 	AppUserId NUMBER NOT NULL,
-  FirstName VARCHAR2(30) NOT NULL,
+	FirstName VARCHAR2(30) NOT NULL,
 	LastName VARCHAR2(30) NOT NULL,
-  Alias VARCHAR2(30),
-  Phone VARCHAR2(30),
+	Alias VARCHAR2(30),
+	Phone VARCHAR2(30),
 	constraint AppUser_PK PRIMARY KEY (AppUserId));
 
 CREATE TABLE AppGroup (
 	AppGroupId NUMBER NOT NULL,
-  GroupName VARCHAR2(30) NOT NULL,
+	GroupName VARCHAR2(30) NOT NULL,
 	CreationDate DATE NOT NULL,
-  GroupDescription VARCHAR2(30),
-  BaseCurrencyId NUMBER NOT NULL,
+	GroupDescription VARCHAR2(30),
+	BaseCurrencyId NUMBER NOT NULL,
 	constraint AppGroup_PK PRIMARY KEY (AppGroupId));
 
 CREATE TABLE Membership (
 	AppUserId NUMBER NOT NULL,
-  AppGroupId NUMBER NOT NULL,
+	AppGroupId NUMBER NOT NULL,
 	JoiningDate DATE NOT NULL,
-  --Here we need a domain
-  MemberRole VARCHAR2(30) NOT NULL,
-  LeavingDate DATE,
+	--Here we need a domain
+	MemberRole VARCHAR2(30) NOT NULL,
+	LeavingDate DATE,
 	constraint Membership_PK PRIMARY KEY (AppUserId, AppGroupId));
 
 CREATE TABLE Expense (
-  ExpenseId NUMBER NOT NULL,
+	ExpenseId NUMBER NOT NULL,
 	AppUserId NUMBER NOT NULL,
-  AppGroupId NUMBER NOT NULL,
-  Amount NUMBER(2) NOT NULL,
-  CurrencyId NUMBER NOT NULL,
+	AppGroupId NUMBER NOT NULL,
+	Amount NUMBER(2) NOT NULL,
+	CurrencyId NUMBER NOT NULL,
 	ExpenseDate DATE NOT NULL,
-  RegistrationDate DATE NOT NULL,
-  --Here we need a domain
-  DivisionType VARCHAR2(30) NOT NULL,
-  CategoryId NUMBER NOT NULL,
+	RegistrationDate DATE NOT NULL,
+	--Here we need a domain
+	DivisionType VARCHAR2(30) NOT NULL,
+	CategoryId NUMBER NOT NULL,
 	constraint Expense_PK PRIMARY KEY (ExpenseId));
 
 CREATE TABLE ParticipationExpense (
-  ExpenseId NUMBER NOT NULL,
+	ExpenseId NUMBER NOT NULL,
 	AppUserId NUMBER NOT NULL,
-  AppGroupId NUMBER NOT NULL,
-  Amount NUMBER(2) NOT NULL,
+	AppGroupId NUMBER NOT NULL,
+	Amount NUMBER(2) NOT NULL,
 	constraint ParticipationExpense_PK PRIMARY KEY (ExpenseId, AppUserId, AppGroupId));
 
 CREATE TABLE Category (
-  CategoryId NUMBER NOT NULL,
-  AppGroupId NUMBER NOT NULL,
-  CategoryName VARCHAR(30) NOT NULL,
+	CategoryId NUMBER NOT NULL,
+	AppGroupId NUMBER NOT NULL,
+	CategoryName VARCHAR(30) NOT NULL,
 	constraint Category_PK PRIMARY KEY (CategoryId));
 
 CREATE TABLE Currency (
-  CurrencyId NUMBER NOT NULL,
-  CurrencyName VARCHAR(30) NOT NULL,
+	CurrencyId NUMBER NOT NULL,
+	CurrencyName VARCHAR(30) NOT NULL,
 	constraint Currency_PK PRIMARY KEY (CurrencyId));
 
 CREATE TABLE ExchangeRate (
-  RateId NUMBER NOT NULL,
-  CurrencyFrom NUMBER NOT NULL,
-  CurrencyTo NUMBER NOT NULL,
-  ExchangeDate DATE NOT NULL,
-  --Here we should determine decimals (or try a different solution)
-  Rate NUMBER(2)
+	RateId NUMBER NOT NULL,
+	CurrencyFrom NUMBER NOT NULL,
+	CurrencyTo NUMBER NOT NULL,
+	ExchangeDate DATE NOT NULL,
+	--Here we should determine decimals (or try a different solution)
+	Rate NUMBER(2)
 	constraint ExchangeRate_PK PRIMARY KEY (RateId));
 
 CREATE TABLE Payment (
-  PaymentId NUMBER NOT NULL,
+	PaymentId NUMBER NOT NULL,
 	PayerId NUMBER NOT NULL,
-  PayeeId NUMBER NOT NULL,
-  AppGroupId NUMBER NOT NULL,
-  Amount NUMBER(2) NOT NULL,
-  CurrencyId NUMBER NOT NULL,
+	PayeeId NUMBER NOT NULL,
+	AppGroupId NUMBER NOT NULL,
+	Amount NUMBER(2) NOT NULL,
+	CurrencyId NUMBER NOT NULL,
 	PaymentDate DATE NOT NULL,
-  Note VARCHAR2(30),
+	Note VARCHAR2(30),
 	constraint Payment_PK PRIMARY KEY (PaymentId));
 
 CREATE TABLE Notification (
-  NotificationId NUMBER NOT NULL,
-  PaymentId NUMBER NOT NULL,
-  RecipientId NUMBER NOT NULL,
-  NotificationText VARCHAR2(30) NOT NULL,
-  NotificationTime TIMESTAMP NOT NULL,
+	NotificationId NUMBER NOT NULL,
+	PaymentId NUMBER NOT NULL,
+	RecipientId NUMBER NOT NULL,
+	NotificationText VARCHAR2(30) NOT NULL,
+	NotificationTime TIMESTAMP NOT NULL,
   --Here we need a domain (I don't think we are supposed to use booleans)
-  IsRead CHAR(1) NOT NULL,
-  constraint Notification_PK PRIMARY KEY (NotificationId))
+	IsRead CHAR(1) NOT NULL,
+	constraint Notification_PK PRIMARY KEY (NotificationId))
 
 CREATE TABLE MessageGroup (
-  MessageGroupId NUMBER NOT NULL,
-  AppGroupId NUMBER NOT NULL,
-  SenderId NUMBER NOT NULL,
-  MessageText VARCHAR2(30) NOT NULL,
-  MessageTime TIMESTAMP NOT NULL,
-  constraint MessageGroup_PK PRIMARY KEY (MessageGroupId))
+	MessageGroupId NUMBER NOT NULL,
+	AppGroupId NUMBER NOT NULL,
+	SenderId NUMBER NOT NULL,
+	MessageText VARCHAR2(30) NOT NULL,
+	MessageTime TIMESTAMP NOT NULL,
+	constraint MessageGroup_PK PRIMARY KEY (MessageGroupId))
 
 CREATE TABLE MessagePrivate (
-  MessagePrivateId NUMBER NOT NULL,
-  AppGroupId NUMBER NOT NULL,
-  SenderId NUMBER NOT NULL,
-  RecipientId NUMBER NOT NULL,
-  MessageText VARCHAR2(30) NOT NULL,
-  MessageTime TIMESTAMP NOT NULL,
-  constraint MessagePrivate_PK PRIMARY KEY (MessagePrivateId))
+	MessagePrivateId NUMBER NOT NULL,
+	AppGroupId NUMBER NOT NULL,
+	SenderId NUMBER NOT NULL,
+	RecipientId NUMBER NOT NULL,
+	MessageText VARCHAR2(30) NOT NULL,
+	MessageTime TIMESTAMP NOT NULL,
+	constraint MessagePrivate_PK PRIMARY KEY (MessagePrivateId))
